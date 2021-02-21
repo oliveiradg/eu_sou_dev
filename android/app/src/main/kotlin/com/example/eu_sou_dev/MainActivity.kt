37f1e7f4ed72623ca13cc7c151b91fde0b184fcc
Joao Oliveira <j.oliveiradg@gmail.com>
2021-02-20 21:02:47 -0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/eu_sou_dev/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.eu_sou_dev
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Center(
+        child: Text('Hello World!!'),
+      ),
+    ),
+  );
+}
+// *** Meu Primeiro código em Flutter; criação do app: Eu sou Dev. 
+//Inicialmente foi feita a base do código e um Hello World para quebrar a 
+//Maldição e foi feita a Identenção da arvore de widgets para ter um 
+//código mais bonito. ***
+
